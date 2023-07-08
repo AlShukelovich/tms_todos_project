c993bb953b1f4af28086f43b7e37090c6b190247
unknown <AShukelovich@gmail.com>
2023-07-08 09:25:31 +0300

    added jwt auth

--- tms_todos_project/tms_todos_project/urls.py
+++ tms_todos_project/tms_todos_project/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todos/', include('todos.urls.urls_for_api')),
     path('todos/forms/', include('todos.urls.urls_for_forms')),
     path('api/v2/', include('todos.urls.urls_for_serializers')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
