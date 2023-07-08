b31a0f2a9a0842033a86a53f97a242ecf26392e7
unknown <AShukelovich@gmail.com>
2023-07-08 08:53:24 +0300

    added pagination

--- tms_todos_project/tms_todos_project/settings.py
+++ tms_todos_project/tms_todos_project/settings.py
@@ -45,6 +45,11 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
